814c39f8115424847b3b701fce72be2f04e1f4b6
“[ida <[ida.t.sylven@gmail.com]>
2021-06-05 21:05:38 +0200

    Updated More Thoughts section with new link

--- src/components/MoreThoughts.js
+++ src/components/MoreThoughts.js
@@ -7,17 +7,15 @@ export const MoreThoughts = () => {
     <section className="more-thoughts-container">
       <h4 className="more-thoughts-heading">MORE THOUGHTS</h4>
       <article className="more-articles article-hidden-desktop">
-        <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer" aria-label="Link will open in new tab">
+        <a href="https://medium.com/p/776c5101e37a/edit" target="_blank" rel="noopener noreferrer" aria-label="Link will open in new tab">
           <p className="more-thoughts-links">MAY 2021. <span className="link-texts">I ❤ CSS! </span><span className="forward-symbol">>></span></p>
         </a>
       </article>
       <article className="more-articles">
-        <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer" aria-label="Link will open in new tab">
+        <a href="https://medium.com/p/bd3285d6c005/edit" target="_blank" rel="noopener noreferrer" aria-label="Link will open in new tab">
           <p className="more-thoughts-links">COMING UP JUNE 2021. <span className="link-texts">My Ambitions: Pinpoints for the future </span><span className="forward-symbol">>></span></p>
         </a>
       </article>
     </section>
   )
 }
-
-// Valid links to articles TBC
